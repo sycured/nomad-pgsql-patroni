c8855c18af5375bbe4e3dbddfd2ec1eda3e5a1a8
sycured <60801403+sycured@users.noreply.github.com>
2022-10-25 11:04:42 -0500

    load postgis exclusively into template_postgis by default

--- files/001_initdb_postgis.sh
+++ files/001_initdb_postgis.sh
@@ -11,13 +11,11 @@ CREATE DATABASE template_postgis;
 UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 EOSQL
 
-# Load PostGIS into both template_database and $POSTGRES_DB
-for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS extensions into $DB"
-	psql --dbname="$DB" <<-'EOSQL'
-		CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
-		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+# Load PostGIS into template_postgis
+echo "Loading PostGIS extensions into template_postgis"
+psql --dbname=template_postgis <<-'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
-done
