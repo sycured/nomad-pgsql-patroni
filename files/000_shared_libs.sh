1216fa82687d8e8d742a9f85d027de12607d30fa
sycured <60801403+sycured@users.noreply.github.com>
2023-01-12 08:56:37 -0500

    add pg_squeeze to shared_preload_libraries

--- files/000_shared_libs.sh
+++ files/000_shared_libs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo "timezone = 'UTC'" >>$PGDATA/postgresql.conf
 
-echo "shared_preload_libraries = 'citus, pgsodium, pg_stat_statements, powa, pg_stat_kcache, pg_qualstats'" >>$PGDATA/postgresql.conf
+echo "shared_preload_libraries = 'citus, pgsodium, pg_stat_statements, powa, pg_stat_kcache, pg_qualstats, pg_squeeze'" >>$PGDATA/postgresql.conf
 
 echo "pg_stat_statements.max = 10000" >>$PGDATA/postgresql.conf
 echo "pg_stat_statements.track = all" >>$PGDATA/postgresql.conf
