a0ff43a69afb7cac69494fbaebd1ea549cf797ab
sycured <60801403+sycured@users.noreply.github.com>
2023-03-04 20:04:36 -0500

    update pgsodium.getkey_script

--- files/000_shared_libs.sh
+++ files/000_shared_libs.sh
@@ -5,3 +5,4 @@ echo "shared_preload_libraries = 'citus, pgsodium, pg_stat_statements, powa, pg_
 
 echo "pg_stat_statements.max = 10000" >>$PGDATA/postgresql.conf
 echo "pg_stat_statements.track = all" >>$PGDATA/postgresql.conf
+echo "pgsodium.getkey_script = '$PGDATA/pgsodium_root.key'" >>$PGDATA/postgresql.conf
